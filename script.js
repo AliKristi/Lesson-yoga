c4bcca1834e4dc822285eb143e1ccce9db46770b
Alina <sokol-a.d@mail.ru>
2021-08-24 14:38:02 +0300

    odal window

--- script.js
+++ script.js
@@ -93,4 +93,31 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('timer', deadLine);
+
+    // Modal window
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        btn = document.querySelectorAll('.description-btn');
+
+    btn.forEach(function(item) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden'; //Запрет прокрутки страницы, пока открыто модальное окно           
+        })
+    })
+    
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden'; //Запрет прокрутки страницы, пока открыто модальное окно
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = ''; // Отмена запрета на прокрутку.
+    });
 });
